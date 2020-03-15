72b8e5b97e454902d44719db9051792cf255aebb
Gary Feng <gary.feng@gmail.com>
2020-03-14 22:25:59 -0400

    Building a pyflink batch example

---
+++ docker/ops-playground-image/python/WordCount.py
@@ -0,0 +1,34 @@
+# https://ci.apache.org/projects/flink/flink-docs-master/getting-started/walkthroughs/python_table_api.html
+
+from pyflink.dataset import ExecutionEnvironment
+from pyflink.table import TableConfig, DataTypes, BatchTableEnvironment
+from pyflink.table.descriptors import Schema, OldCsv, FileSystem
+
+exec_env = ExecutionEnvironment.get_execution_environment()
+exec_env.set_parallelism(1)
+t_config = TableConfig()
+t_env = BatchTableEnvironment.create(exec_env, t_config)
+
+t_env.connect(FileSystem().path('/tmp/input')) \
+    .with_format(OldCsv()
+                 .field('word', DataTypes.STRING())) \
+    .with_schema(Schema()
+                 .field('word', DataTypes.STRING())) \
+    .create_temporary_table('mySource')
+
+t_env.connect(FileSystem().path('/tmp/output')) \
+    .with_format(OldCsv()
+                 .field_delimiter('\t')
+                 .field('word', DataTypes.STRING())
+                 .field('count', DataTypes.BIGINT())) \
+    .with_schema(Schema()
+                 .field('word', DataTypes.STRING())
+                 .field('count', DataTypes.BIGINT())) \
+    .create_temporary_table('mySink')
+
+t_env.from_path('mySource') \
+    .group_by('word') \
+    .select('word, count(1)') \
+    .insert_into('mySink')
+
+t_env.execute("tutorial_job")
