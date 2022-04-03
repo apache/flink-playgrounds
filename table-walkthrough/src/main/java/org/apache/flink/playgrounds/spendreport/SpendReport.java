563320d7c36eaa3fa83b8500f0121be383e16558
wang.jin <>
2022-04-03 14:28:26 +0800

    fulfill code

--- table-walkthrough/src/main/java/org/apache/flink/playgrounds/spendreport/SpendReport.java
+++ table-walkthrough/src/main/java/org/apache/flink/playgrounds/spendreport/SpendReport.java
@@ -29,7 +29,15 @@ import static org.apache.flink.table.api.Expressions.*;
 public class SpendReport {
 
     public static Table report(Table transactions) {
-        throw new UnimplementedException();
+        return transactions.select(
+            $("account_id"),
+            $("transaction_time").floor(TimeIntervalUnit.HOUR).as("log_ts"),
+            $("amount"))
+        .groupBy($("account_id"), $("log_ts"))
+        .select(
+            $("account_id"),
+            $("log_ts"),
+            $("amount").sum().as("amount"));
     }
 
     public static void main(String[] args) throws Exception {
